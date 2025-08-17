7fa33d3b8502a46967494174f71a3e454b2d8169
corey-stidston <corey.stidston@outlook.com>
2025-08-18 08:14:30 +1000

    refactor: lift up aged brie conditional

--- GildedRose/Item.cs
+++ GildedRose/Item.cs
@@ -9,6 +9,91 @@ public class Item
     public void UpdateQuality()
     {
         var isAgedBrie = Name == "Aged Brie";
+
+        if (isAgedBrie)
+        {
+            if (Quality < 50)
+            {
+                ++Quality;
+            }
+
+            --SellIn;
+
+            if (SellIn < 0)
+            {
+                if (Quality < 50)
+                {
+                    ++Quality;
+                }
+            }
+        }
+        else
+        {
+            if (Name != "Backstage passes to a TAFKAL80ETC concert")
+            {
+                if (Quality > 0)
+                {
+                    if (Name != "Sulfuras, Hand of Ragnaros")
+                    {
+                        --Quality;
+                    }
+                }
+            }
+            else
+            {
+                if (Quality < 50)
+                {
+                    ++Quality;
+
+                    if (Name == "Backstage passes to a TAFKAL80ETC concert")
+                    {
+                        if (SellIn < 11)
+                        {
+                            if (Quality < 50)
+                            {
+                                ++Quality;
+                            }
+                        }
+
+                        if (SellIn < 6)
+                        {
+                            if (Quality < 50)
+                            {
+                                ++Quality;
+                            }
+                        }
+                    }
+                }
+            }
+
+            if (Name != "Sulfuras, Hand of Ragnaros")
+            {
+                --SellIn;
+            }
+
+            if (SellIn < 0)
+            {
+                if (Name != "Backstage passes to a TAFKAL80ETC concert")
+                {
+                    if (Quality > 0)
+                    {
+                        if (Name != "Sulfuras, Hand of Ragnaros")
+                        {
+                            --Quality;
+                        }
+                    }
+                }
+                else
+                {
+                    Quality = 0;
+                }
+            }
+
+        }
+    }
+
+    private void NewMethod(bool isAgedBrie)
+    {
         if (!isAgedBrie && Name != "Backstage passes to a TAFKAL80ETC concert")
         {
             if (Quality > 0)
@@ -44,7 +129,7 @@ public class Item
                     }
                 }
             }
-        }       
+        }
 
         if (Name != "Sulfuras, Hand of Ragnaros")
         {
@@ -79,5 +164,4 @@ public class Item
             }
         }
     }
-
 }
